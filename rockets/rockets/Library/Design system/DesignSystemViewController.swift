9740a8878e5fd507e7e28d69f2d2b29e43488f13
Artem Tebenkov <artem.tebenkov@icloud.com>
2024-07-29 18:57:25 +0400

    Prepare for creating design system

---
+++ rockets/rockets/Library/Design system/DesignSystemViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DesignSystemViewController.swift
+//  rockets
+//
+//  Created by Artem Tebenkov on 29.07.2024.
+//
+
+import UIKit
+
+final class DesignSystemViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = AppTheme.Color.secondary
+    }
+}
